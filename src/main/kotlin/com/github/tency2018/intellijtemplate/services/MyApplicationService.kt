d9125c3353a5ea8748d96ff21ef41249fbfe2e83
GitHub Action <action@github.com>
2020-07-31 09:08:34 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/tency2018/intellijtemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.tency2018.intellijtemplate.services
+
+import com.github.tency2018.intellijtemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/tency2018/intellijtemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tency2018/intellijtemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tency2018.intellijtemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tency2018.intellijtemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
